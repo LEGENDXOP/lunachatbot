1be42e144100729a6b4249fdee8a02c95137e307
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-02-27 16:06:22 +0530

    Update ChatBot_.py

--- Luna/modules/ChatBot_.py
+++ Luna/modules/ChatBot_.py
@@ -3,7 +3,7 @@ from Luna import tbot
 from Luna.events import register
 url = "https://iamai.p.rapidapi.com/ask"
 
-@register(pattern="^/test (.*)")
+@register(pattern="^/q (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -23,35 +23,3 @@ async def hmm(event):
   result = (lodu['message']['text'])
   await event.reply(result)
 
-import requests
-from telethon import events
-from Luna import tbot, BOT_ID
-
-
-@tbot.on(events.NewMessage(pattern=None))
-async def _(event):
-    if event.is_group:
-        pass
-    else:
-        return
-    message = str(event.text)
-    reply_msg = await event.get_reply_message()
-    if reply_msg:
-       if reply_msg.sender_id == BOT_ID:
-        test = event.pattern_match.group(1)
-        r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
-        k = f"({r})"
-        new_string = k.replace("(", "{")
-        lol = new_string.replace(")","}")
-        payload = lol
-        headers = {
-      'content-type': "application/json",
-      'x-forwarded-for': "<user's ip>",
-      'x-rapidapi-key': "fef481fee3mshf99983bfc650decp104100jsnbad6ddb2c846",
-      'x-rapidapi-host': "iamai.p.rapidapi.com"
-      }
-
-        response = requests.request("POST", url, data=payload, headers=headers)
-        lodu = response.json()
-        result = (lodu['message']['text'])
-        await event.reply(result)
